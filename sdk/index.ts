bcbde8d5b676b0cfb0c68127ff077bf0c8d07216
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-07-04 16:28:31 +0200

    create sdk/ barrel export with @navalcode/sdk alias in tsconfig paths

---
+++ sdk/index.ts
@@ -0,0 +1,6 @@
+export { Brain } from "../src/classes/Brain";
+export { Board } from "../src/classes/Board";
+export { Strategy } from "../src/classes/Strategy";
+export type { Direction, BoatPlacement } from "../src/classes/Strategy";
+export { State } from "../src/types/State";
+export { Boats } from "../src/types/Boats";
